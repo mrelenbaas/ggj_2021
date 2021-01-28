b4db1baa5f069cf072c238b9de386510e3e640c1
Martian__Knight <mr.elenbaas@gmail.com>
2021-01-28 16:00:10 -0600

    Updated Android with clouds.

--- java/GGJ2021/app/src/main/java/com/bradleyelenbaas/ggj2021/MainActivity.java
+++ java/GGJ2021/app/src/main/java/com/bradleyelenbaas/ggj2021/MainActivity.java
@@ -3,12 +3,21 @@ package com.bradleyelenbaas.ggj2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CanvasView customCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customCanvas = (CanvasView) findViewById(R.id.signature_canvas);
+    }
+
+    public void clearCanvas(View v) {
+        customCanvas.clearCanvas();
     }
 }
